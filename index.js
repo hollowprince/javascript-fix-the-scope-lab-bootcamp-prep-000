6e702281f3e0deac42cb992b22ba7d21b992a901
Cam Holloway <hollowprince@outlook.com>
2018-05-15 19:17:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function add2(n) {
   
 }
 
-var funkyFunction = function() {
-  return function() {
+var funkyFunction = function funky() {
+  return function funky() {
     return "FUNKY!"
   }
 }
